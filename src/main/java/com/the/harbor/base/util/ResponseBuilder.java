5d399b41c97a57819ea39e9694f06bbef8f40514
zhangchao2 <zhangchao2@asiainfo.com>
2016-05-26 19:43:08 +0800

    修改返回对象

---
+++ src/main/java/com/the/harbor/base/util/ResponseBuilder.java
@@ -0,0 +1,20 @@
+package com.the.harbor.base.util;
+
+import com.the.harbor.base.constants.ExceptCodeConstants;
+import com.the.harbor.base.vo.Response;
+import com.the.harbor.base.vo.ResponseHeader;
+
+public final class ResponseBuilder {
+
+    private ResponseBuilder() {
+
+    }
+
+    public static Response buildSuccessResponse(String message) {
+        ResponseHeader responseHeader = new ResponseHeader(ExceptCodeConstants.SUCCESS, message);
+        Response r = new Response();
+        r.setResponseHeader(responseHeader);
+        return r;
+    }
+
+}
