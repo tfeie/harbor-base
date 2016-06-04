49b59e2edbbe6961c2dd10f698e0237bdbea3641
zhangchao2 <zhangchao2@asiainfo.com>
2016-06-04 23:18:12 +0800

    增加参数校验失败

--- src/main/java/com/the/harbor/base/constants/ExceptCodeConstants.java
+++ src/main/java/com/the/harbor/base/constants/ExceptCodeConstants.java
@@ -1,15 +1,18 @@
 package com.the.harbor.base.constants;
 
 public final class ExceptCodeConstants {
-    private ExceptCodeConstants() {
-    }
-  
-    // 处理成功[业务处理成功]
-    public static final String SUCCESS = "000000";
+	private ExceptCodeConstants() {
+	}
 
-    // 系统级异常[其它系统级异常，未知异常]
-    public static final String SYSTEM_ERROR = "999999";
+	// 处理成功[业务处理成功]
+	public static final String SUCCESS = "000000";
 
-    public static final String PARAM_IS_NULL = "888888";
+	// 系统级异常[其它系统级异常，未知异常]
+	public static final String SYSTEM_ERROR = "999999";
+
+	public static final String PARAM_IS_NULL = "888888";
+
+	// 参数校验失败
+	public static final String VALID_ERROR = "777777";
 
 }
