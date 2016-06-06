1f5db3b99fbe4096e7580d471e93138de85799ab
zhangchao2 <zhangchao2@asiainfo.com>
2016-06-06 10:14:18 +0800

    修改认证状态

--- src/main/java/com/the/harbor/base/enumeration/hyuser/UserStatus.java
+++ src/main/java/com/the/harbor/base/enumeration/hyuser/UserStatus.java
@@ -4,32 +4,34 @@ import com.the.harbor.base.enumeration.base.Behaviour;
 
 public enum UserStatus implements Behaviour {
 
-    AUTHORIZED("20", "已认证"),
+	AUTHORIZED_SUCCESS("20", "已认证"),
 
-    UNAUTHORIZED("10", "未认证");
+	UNAUTHORIZED("10", "未认证"),
 
-    private String value;
+	AUTHORIZED_FAILURE("11", "认证不通过");
 
-    private String desc;
+	private String value;
 
-    private UserStatus(String value, String desc) {
-        this.value = value;
-        this.desc = desc;
-    }
+	private String desc;
 
-    @Override
-    public String toString() {
-        return this.value + " is " + this.desc;
-    }
+	private UserStatus(String value, String desc) {
+		this.value = value;
+		this.desc = desc;
+	}
 
-    @Override
-    public String getValue() {
-        return this.value;
-    }
+	@Override
+	public String toString() {
+		return this.value + " is " + this.desc;
+	}
 
-    @Override
-    public String getDesc() {
-        return this.desc;
-    }
+	@Override
+	public String getValue() {
+		return this.value;
+	}
+
+	@Override
+	public String getDesc() {
+		return this.desc;
+	}
 
 }
