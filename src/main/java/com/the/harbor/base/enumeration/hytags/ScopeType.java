ae875d856eaeab1965e7b21a8df1fbb3e501b732
zhangchao2 <zhangchao2@asiainfo.com>
2016-06-09 21:48:42 +0800

    增加标签枚举

---
+++ src/main/java/com/the/harbor/base/enumeration/hytags/ScopeType.java
@@ -0,0 +1,37 @@
+package com.the.harbor.base.enumeration.hytags;
+
+import com.the.harbor.base.enumeration.base.Behaviour;
+
+public enum ScopeType implements Behaviour {
+
+	USER("10", "适用用户"),
+
+	GO("11", "适用GROUP和One On One活动"),
+
+	BE("12", "适用BE");
+
+	private String value;
+
+	private String desc;
+
+	private ScopeType(String value, String desc) {
+		this.value = value;
+		this.desc = desc;
+	}
+
+	@Override
+	public String toString() {
+		return this.value + " is " + this.desc;
+	}
+
+	@Override
+	public String getValue() {
+		return this.value;
+	}
+
+	@Override
+	public String getDesc() {
+		return this.desc;
+	}
+
+}
---
+++ src/main/java/com/the/harbor/base/enumeration/hytags/TagCat.java
@@ -0,0 +1,35 @@
+package com.the.harbor.base.enumeration.hytags;
+
+import com.the.harbor.base.enumeration.base.Behaviour;
+
+public enum TagCat implements Behaviour {
+
+	USER("10", "系统内置标签"),
+
+	CUSTOMIZED("11", "自定义标签");
+
+	private String value;
+
+	private String desc;
+
+	private TagCat(String value, String desc) {
+		this.value = value;
+		this.desc = desc;
+	}
+
+	@Override
+	public String toString() {
+		return this.value + " is " + this.desc;
+	}
+
+	@Override
+	public String getValue() {
+		return this.value;
+	}
+
+	@Override
+	public String getDesc() {
+		return this.desc;
+	}
+
+}
