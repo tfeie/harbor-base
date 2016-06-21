6cc5ad201237ba2624ebe0422c4b52bfb9d5acb0
zhangchao2 <zhangchao2@asiainfo.com>
2016-06-21 14:20:02 +0800

    1

--- src/main/java/com/the/harbor/base/exception/BusinessException.java
+++ src/main/java/com/the/harbor/base/exception/BusinessException.java
@@ -2,12 +2,40 @@ package com.the.harbor.base.exception;
 
 public class BusinessException extends GenericException {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public BusinessException(String errorCode, String errorMessage) {
-        super(errorMessage);
-        this.errorCode = errorCode;
-        this.errorMessage = errorMessage;
-    }
+	private String title;
+
+	private String url;
+
+	public BusinessException(String errorCode, String errorMessage, String title) {
+		super(errorMessage);
+		this.errorCode = errorCode;
+		this.errorMessage = errorMessage;
+	}
+
+	public BusinessException(String errorCode, String errorMessage, String title, String url) {
+		super(errorMessage);
+		this.errorCode = errorCode;
+		this.errorMessage = errorMessage;
+		this.title = title;
+		this.url = url;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
 
 }
