54aa59049efc53a466b17af706daa6c7ab9601a2
zhangchao2 <zhangchao2@asiainfo.com>
2016-06-24 15:27:56 +0800

    1

--- src/main/java/com/the/harbor/base/vo/MNSBody.java
+++ src/main/java/com/the/harbor/base/vo/MNSBody.java
@@ -4,11 +4,34 @@ import java.io.Serializable;
 
 /**
  * 消息体，业务消息继承此类
+ * 
  * @author zhangchao
  *
  */
 public class MNSBody implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
+	// 消息标识
+	private String mqId;
+
+	// 消息类型
+	private String mqType;
+
+	public String getMqId() {
+		return mqId;
+	}
+
+	public void setMqId(String mqId) {
+		this.mqId = mqId;
+	}
+
+	public String getMqType() {
+		return mqType;
+	}
+
+	public void setMqType(String mqType) {
+		this.mqType = mqType;
+	}
 
 }
