b52b0633a9cdfeceadd0d60cc017d513331d53ca
zhangchao2 <zhangchao2@asiainfo.com>
2016-07-23 10:37:17 +0800

    1

--- src/main/java/com/the/harbor/base/enumeration/hyuser/UserInviteStatus.java
+++ src/main/java/com/the/harbor/base/enumeration/hyuser/UserInviteStatus.java
@@ -4,21 +4,19 @@ import com.the.harbor.base.enumeration.base.Behaviour;
 
 public enum UserInviteStatus implements Behaviour {
 
-	INVITE_VALID("0", "生效"),
-	
-	INVITE_USING("1", "正在使用"),
+	NOT_USE("0", "可使用"),
 
-	INVITE_INVALID("2", "失效");
-	
+	USED("1", "已使用");
+
 	private String value;
 
 	private String desc;
-	
+
 	private UserInviteStatus(String value, String desc) {
 		this.value = value;
 		this.desc = desc;
 	}
-	
+
 	@Override
 	public String getValue() {
 		return this.value;
