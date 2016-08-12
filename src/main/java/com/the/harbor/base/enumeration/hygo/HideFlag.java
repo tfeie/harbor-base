3f7e33643b43b4a24cf1f798a0eb81ca971081a3
zhangchao2 <zhangchao2@asiainfo.com>
2016-08-12 23:39:52 +0800

    隐藏标记

---
+++ src/main/java/com/the/harbor/base/enumeration/hygo/HideFlag.java
@@ -0,0 +1,35 @@
+package com.the.harbor.base.enumeration.hygo;
+
+import com.the.harbor.base.enumeration.base.Behaviour;
+
+public enum HideFlag implements Behaviour {
+
+	YES("1", "隐藏"),
+
+	NO("0", "显示");
+
+	private String value;
+
+	private String desc;
+
+	private HideFlag(String value, String desc) {
+		this.value = value;
+		this.desc = desc;
+	}
+
+	@Override
+	public String toString() {
+		return this.value + " is " + this.desc;
+	}
+
+	@Override
+	public String getValue() {
+		return this.value;
+	}
+
+	@Override
+	public String getDesc() {
+		return this.desc;
+	}
+
+}
