4f4a89cc1f33aa10085a194ff2f7ccf6d2ee9cfa
zhangchao2 <zhangchao2@asiainfo.com>
2016-08-15 15:41:33 +0800

    评论状态

---
+++ src/main/java/com/the/harbor/base/enumeration/hygocomments/Status.java
@@ -0,0 +1,37 @@
+package com.the.harbor.base.enumeration.hygocomments;
+
+import com.the.harbor.base.enumeration.base.Behaviour;
+
+public enum Status implements Behaviour {
+
+	NORMAL("normal", "正常"),
+	
+	HIDE("hide", "隐藏"),
+
+	DELETED("deleted", "删除");
+
+	private String value;
+
+	private String desc;
+
+	private Status(String value, String desc) {
+		this.value = value;
+		this.desc = desc;
+	}
+
+	@Override
+	public String toString() {
+		return this.value + " is " + this.desc;
+	}
+
+	@Override
+	public String getValue() {
+		return this.value;
+	}
+
+	@Override
+	public String getDesc() {
+		return this.desc;
+	}
+
+}
